59183158b6c6e318905cdc7f14c6fef3f67e44bd
KamiwatariTakeru <takeru.shonan@gmail.com>
2024-07-31 04:15:53 +0900

    enviromental variable in [nextAuth].ts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      API_URL: process.env.API_URL || 'https://spots-post-backend.fly.dev',
+      API_URL: process.env.API_URL || 'https://spots-post-api.fly.dev',
     },
   };
 
