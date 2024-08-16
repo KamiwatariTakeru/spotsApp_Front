d65c76923ab6239d2fb2242ade840fe6b0458007
KamiwatariTakeru <takeru.shonan@gmail.com>
2024-08-16 23:56:51 +0900

    next.config.jsのenvセクション削除

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      API_URL_FROM_SERVER_SIDE: process.env.API_URL_FROM_SERVER_SIDE,
-      API_URL_FROM_CLIENT_SIDE: process.env.API_URL_FROM_CLIENT_SIDE,
-    },
   };
 
   module.exports = nextConfig;
